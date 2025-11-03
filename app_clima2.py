38714310a059718eb7d3b92748e6c5a74a6f68e5
Jimena Pereyra <jpereyra@geanet.com.ar>
2025-11-02 19:57:13 -0500

    Modificacion para CSFR

--- app_clima2.py
+++ app_clima2.py
@@ -5,7 +5,7 @@ import requests
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY']=os.environ.get('SECRET_KEY', 'dev_secret_key')
+app.config['SECRET_KEY']=os.environ['SECRET_KEY']
 
 csrf = CSRFprotect(app)
 
