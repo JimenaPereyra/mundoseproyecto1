ba80fa945d5ba88fd2d00c666af238a03624df0b
Jimena Pereyra <jpereyra@geanet.com.ar>
2025-11-29 16:40:52 -0500

    Agregado de infra en Terraform

---
+++ terraform/variables.tf
@@ -0,0 +1,32 @@
+variable "aws_region" {
+  type = string
+  default = "us-east-1"
+}
+
+
+variable "dockerhub_username" {
+  type = string
+}
+
+variable "image_tag" {
+  type = string
+  description = "Tag de la docker image para deploy"
+}
+
+variable "weather_api_key" {
+  type = string
+  sensitive = true
+}
+
+variable "instance_type" {
+  description = "Tipo de instancia EC2"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "ami_id" {
+  description = "ID de la imagen Ubuntu 22.04"
+  type        = string
+  default     = "ami-0c398cb65a93047f2”
+}
+
