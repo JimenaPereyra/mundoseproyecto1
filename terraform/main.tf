b0c20922fd6cace74fbfb024529df386856a2f99
Jimena Pereyra <jpereyra@geanet.com.ar>
2025-11-29 21:45:00 -0500

    modificacion de archivos terraform2

--- terraform/main.tf
+++ terraform/main.tf
@@ -66,7 +66,7 @@ resource "aws_instance" "app" {
   subnet_id = aws_subnet.public.id
   associate_public_ip_address = true
   vpc_security_group_ids = [aws_security_group.sg.id]
-  key_name = proyectofinal
+  key_name = "proyectofinal"
 
   user_data = <<-EOF
     #!/bin/bash
