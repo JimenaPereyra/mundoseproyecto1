8a2e07f576d21248d5e3ea6d511415f5ae261af8
JimenaPereyra <156346572+JimenaPereyra@users.noreply.github.com>
2025-12-01 03:36:11 -0300

    Update main.tf puerto prometheus

--- terraform/main.tf
+++ terraform/main.tf
@@ -41,6 +41,8 @@ resource "aws_security_group" "sg" {
     protocol  = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+# Puerto SSH
   ingress {
     from_port = 22
     to_port   = 22
@@ -55,11 +57,19 @@ resource "aws_security_group" "sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-
+  # Puerto Grafana
   ingress {
     from_port = 3000
     to_port   = 3000
     protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+# Puerto Prometheus
+  ingress {
+    description = "Prometheus"
+    from_port   = 9090
+    to_port     = 9090
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
