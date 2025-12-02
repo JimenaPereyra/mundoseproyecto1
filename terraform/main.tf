c69ed75634c6190da1bc08ff5fc8821b7fb22091
Jimena Pereyra <jpereyra@geanet.com.ar>
2025-12-01 23:37:58 -0500

    Modificacion de orden de prometheus y grafana2

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,4 @@
-data "aws_availability_zones" "azs" {}
+
 
 resource "aws_vpc" "vpc" {
   cidr_block = "10.0.0.0/16"
@@ -9,7 +9,7 @@ resource "aws_subnet" "public" {
   vpc_id = aws_vpc.vpc.id
   cidr_block = "10.0.1.0/24"
   map_public_ip_on_launch = true
-  availability_zone = data.aws_availability_zones.azs.names[0]
+  availability_zone = var.aws_region
   tags = { Name = "clima-subnet" }
 }
 
