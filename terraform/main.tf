1974015601bec6906da5fbe955344de34e9dff8b
Jimena Pereyra <jpereyra@geanet.com.ar>
2025-12-02 00:23:29 -0500

    Modificacion de orden de prometheus y grafana6

--- terraform/main.tf
+++ terraform/main.tf
@@ -106,7 +106,7 @@ usermod -aG docker ubuntu
 # 3 Instalar Docker Compose v2
 # ===============================
 DOCKER_COMPOSE_VERSION="v2.20.2"
-curl -L "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/\${DOCKER_COMPOSE_VERSION}/docker-compose-\$(uname -s)-\$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
 # Verificar instalación
